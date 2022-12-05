c7d1af9ad7ec93ded424b0c2826794e1801cb3bf
ttherezi <titouan.therezien@edu.univ-fcomte.fr>
2022-12-05 08:05:35 +0100

    committed

--- StuckWin.java
+++ StuckWin.java
@@ -18,13 +18,27 @@ public class StuckWin {
             {'-', '-', '-', '-', 'R', 'R', 'R', 'R'},
             {'-', '-', '-', '.', 'R', 'R', 'R', 'R'},
             {'-', '-', '.', '.', '.', 'R', 'R', 'R'},
-            {'-', 'B', 'B', '.', '.', '.', 'R', 'R'},
-            {'-', 'B', 'B', 'B', '.', '.', '.', '-'},
+            {'-', 'B', 'B', '.', '.', '.', 'R', 'B'},
+            {'-', 'B', 'B', 'B', '.', '.', 'B', '-'},
             {'-', 'B', 'B', 'B', 'B', '.', '-', '-'},
-            {'-', 'B', 'B', 'B', 'B', '-', '-', '-'},
+            {'-', 'B', '.', 'R', 'B', '-', '-', '-'},
     };
 
-    
+    String [][] Tableau = {
+      {"","","","07","","",""},
+      {"","","06","","17","",""},
+      {"","05","","16","","27",""},
+      {"04","","15","","26","","37"},
+      {"","14","","25","","36",""},
+      {"13","","24","","35","","46"},
+      {"","23","","34","","45",""},
+      {"22","","33","","44","","55"},
+      {"","32","","43","","54",""},
+      {"31","","42","","53","","64"},
+      {"","41","","52","","63",""},
+      {"","","51","","62","",""},
+      {"","","","61","","",""}
+    };
     /**
      * Déplace un pion ou simule son déplacement
      * @param couleur couleur du pion à déplacer
@@ -49,9 +63,102 @@ public class StuckWin {
      * @return tableau des trois positions jouables par le pion (redondance possible sur les bords)
      */
     String[] possibleDests(char couleur, int idLettre, int idCol){
-      // votre code ici. Supprimer la ligne ci-dessous.
-      throw new java.lang.UnsupportedOperationException("à compléter");
+      
+      if (couleur == 'R')
+      {
+        String [] posPossible  = { "" + idLettre + (idCol-1) ,"" + (idLettre+1) + (idCol), "" + (idLettre+1) + (idCol-1) };
+      
+        for (int i =0;i<posPossible.length;i++)
+        {
+          char tmp = posPossible[i].charAt(0);
+          int lettre = Character.getNumericValue(tmp);
+          tmp = posPossible[i].charAt(1);
+          int chiffre = Character.getNumericValue(tmp);
+
+
+          if ((lettre>=0 && lettre<7) && (chiffre>=0 && chiffre<8))
+          {
+            if( state[lettre][chiffre] != '.' )
+            {
+                posPossible[i] = "-";
+            }
+          }
+          else
+          {
+              posPossible[i] = "Out";
+          }
+            
+          
+
+        }
+      return posPossible;
+      
+      }
+      else if(couleur == 'B')
+      {
+        String [] posPossible  = { "" + idLettre + (idCol+1) ,"" + (idLettre-1) + (idCol), "" + (idLettre-1) + (idCol-1) };
+      
+        for (int i =0;i<posPossible.length;i++)
+        {
+          char tmp = posPossible[i].charAt(0);
+          int lettre = Character.getNumericValue(tmp);
+          tmp = posPossible[i].charAt(1);
+          int chiffre = Character.getNumericValue(tmp);
+
+          
+          if ((lettre>=0 && lettre<7) && (chiffre>=0 && chiffre<8))
+          {
+            if( state[lettre][chiffre] != '.' )
+            {
+                posPossible[i] = "-";
+            }
+          }
+          else
+          {
+              posPossible[i] = "Out";
+          }
+
+        }
+        return posPossible;
+      
+      }
+      else
+      {
+        String posPossible [] = {"False"};
+        return posPossible;
+      }
+
+
     }
+
+/**
+ * Traducteur de l'idLettre, entre String valeur forme "E2" sortie String "42"
+ * @param valeur
+ * @return
+ */
+    String TradIdLettre(String  valeur )
+    {
+      char tmp = valeur.charAt(0);
+      char tmp1 = valeur.charAt(1);
+      tmp = Character.toUpperCase(tmp);
+      
+      String valide = "ABCDEFG";
+      if (valide.contains(String.valueOf(tmp))) {
+        valeur = "Out";
+        return valeur;
+      }
+
+
+
+      int val = tmp;
+      val = val-65;
+      tmp =(char)val;
+      return ""+tmp+tmp1;
+      
+    }
+    
+
+
 
     /**
      * Affiche le plateau de jeu dans la configuration portée par
@@ -59,21 +166,7 @@ public class StuckWin {
      */
     void affiche() {
 
-      String [][] Tableau = {
-        {"","","","07","","",""},
-        {"","","06","","17","",""},
-        {"","05","","16","","27",""},
-        {"04","","15","","26","","37"},
-        {"","14","","25","","36",""},
-        {"13","","24","","35","","46"},
-        {"","23","","34","","45",""},
-        {"22","","33","","44","","55"},
-        {"","32","","43","","54",""},
-        {"31","","42","","53","","64"},
-        {"","41","","52","","63",""},
-        {"","","51","","62","",""},
-        {"","","","61","","",""}
-      };
+     
 
       int lettre;
       int chiffre;
@@ -116,7 +209,7 @@ public class StuckWin {
           
 
         }
-        System.out.println("");;
+        System.out.println("");
       }
 
 
@@ -154,6 +247,13 @@ public class StuckWin {
                 src = input.next();
                 dst = input.next();
                 System.out.println(src + "->" + dst);
+
+                //Test TradIdLettre
+                src = TradIdLettre(src);
+                dst = TradIdLettre(dst);
+                System.out.println(src + "->" + dst);
+                //Fin de Test
+
                 break;
             case 'R':
                 System.out.println("Mouvement " + couleur);
@@ -193,9 +293,25 @@ public class StuckWin {
         do {
               // séquence pour Bleu ou rouge
               jeu.affiche();
+
+
+              //Test Pour Possible dest
+              String [] pos  = jeu.possibleDests('R',6,3);
+              for (int i = 0;i<pos.length;i++)
+              {
+                System.out.println(pos[i]);
+
+              }
+
+
+
+
+
+
               do {
                   status = Result.EXIT;
                   reponse = jeu.jouer(curCouleur);
+                  
                   src = reponse[0];
                   dest = reponse[1];
                   if("q".equals(src))
